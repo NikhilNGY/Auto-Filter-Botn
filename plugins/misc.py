d63c294af8245c5e72c13ebbd928fb4d14d43df4
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-18 18:08:59 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -8,12 +8,22 @@ from datetime import datetime
 import os
 
 
+# ---------------- /id Command ---------------- #
 @Client.on_message(filters.command('id'))
 async def showid(client, message):
     chat_type = message.chat.type
-    replied_to_msg = bool(message.reply_to_message)
+    replied_to_msg = message.reply_to_message
+
     if replied_to_msg:
-        return await message.reply_text(f"""The forwarded message channel {replied_to_msg.chat.title}'s id is, <code>{replied_to_msg.chat.id}</code>.""")
+        if replied_to_msg.forward_from_chat:
+            return await message.reply_text(
+                f"The forwarded message is from channel <b>{replied_to_msg.forward_from_chat.title}</b>\nID: <code>{replied_to_msg.forward_from_chat.id}</code>"
+            )
+        else:
+            return await message.reply_text(
+                f"The message is from user <b>{replied_to_msg.from_user.first_name}</b>\nID: <code>{replied_to_msg.from_user.id}</code>"
+            )
+
     if chat_type == enums.ChatType.PRIVATE:
         await message.reply_text(f'★ User ID: <code>{message.from_user.id}</code>')
 
@@ -24,128 +34,126 @@ async def showid(client, message):
         await message.reply_text(f'★ Channel ID: <code>{message.chat.id}</code>')
 
 
+# ---------------- /speedtest Command ---------------- #
 @Client.on_message(filters.command('speedtest') & filters.user(ADMINS))
 async def speedtest(client, message):
-    #from - https://github.com/weebzone/WZML-X/blob/master/bot/modules/speedtest.py
-    msg = await message.reply_text("Initiating Speedtest...")
+    msg = await message.reply_text("Running Speedtest... ⏳")
     try:
         speed = Speedtest()
         speed.get_best_server()
+        speed.download()
+        speed.upload()
+        speed.results.share()
+        result = speed.results.dict()
     except (ConfigRetrievalError, SpeedtestBestServerFailure):
-        await msg.edit("Can't connect to Server at the Moment, Try Again Later !")
-        return
-    speed.download()
-    speed.upload()
-    speed.results.share()
-    result = speed.results.dict()
-    photo = result['share']
-    text = f'''
-➲ <b>SPEEDTEST INFO</b>
+        return await msg.edit("❌ Can't connect to Speedtest server. Try again later.")
+    except Exception as e:
+        return await msg.edit(f"❌ Error: {e}")
+
+    # Formatting result
+    timestamp = result.get('timestamp')
+    time_str = timestamp
+    if timestamp:
+        try:
+            time_str = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S")
+        except:
+            pass
+
+    text = f"""
+<b>➲ SPEEDTEST RESULTS</b>
 ┠ <b>Upload:</b> <code>{get_size(result['upload'])}/s</code>
-┠ <b>Download:</b>  <code>{get_size(result['download'])}/s</code>
+┠ <b>Download:</b> <code>{get_size(result['download'])}/s</code>
 ┠ <b>Ping:</b> <code>{result['ping']} ms</code>
-┠ <b>Time:</b> <code>{datetime.strptime(result['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S")}</code>
+┠ <b>Time:</b> <code>{time_str}</code>
 ┠ <b>Data Sent:</b> <code>{get_size(int(result['bytes_sent']))}</code>
 ┖ <b>Data Received:</b> <code>{get_size(int(result['bytes_received']))}</code>
 
-➲ <b>SPEEDTEST SERVER</b>
+<b>➲ SERVER INFO</b>
 ┠ <b>Name:</b> <code>{result['server']['name']}</code>
 ┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
 ┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
-┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
-┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+┠ <b>Latency:</b> <code>{result['server']['latency']} ms</code>
+┠ <b>Location:</b> <code>{result['server']['lat']}, {result['server']['lon']}</code>
 
-➲ <b>CLIENT DETAILS</b>
-┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
-┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
-┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
+<b>➲ CLIENT INFO</b>
+┠ <b>IP:</b> <code>{result['client']['ip']}</code>
+┠ <b>Location:</b> <code>{result['client']['lat']}, {result['client']['lon']}</code>
 ┠ <b>Country:</b> <code>{result['client']['country']}</code>
 ┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
 ┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
-'''
-    await message.reply_photo(photo=photo, caption=text)
+"""
+
+    await message.reply_photo(photo=result['share'], caption=text)
     await msg.delete()
 
 
+# ---------------- /info Command ---------------- #
 @Client.on_message(filters.command("info"))
 async def who_is(client, message):
-    status_message = await message.reply_text(
-        "Fetching user info..."
-    )
+    status_message = await message.reply_text("Fetching user info... ⏳")
+
+    # Determine user
     if message.reply_to_message:
         from_user_id = message.reply_to_message.from_user.id
     elif len(message.command) > 1:
         from_user_id = message.command[1]
     else:
         from_user_id = message.from_user.id
+
     try:
-        from_user = await client.get_users(from_user_id)
-    except Exception as error:
-        await status_message.edit(f'Error: {error}')
+        user = await client.get_users(from_user_id)
+    except Exception as e:
+        await status_message.edit(f"❌ Error: {e}")
         return
 
-    message_out_str = ""
-    message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
-    last_name = from_user.last_name or 'Not have'
-    message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
-    message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
-    username = f'@{from_user.username}' if from_user.username else 'Not have'
-    dc_id = from_user.dc_id or "Not found"
-    message_out_str += f"<b>➲Data Centre:</b> <code>{dc_id}</code>\n"
-    message_out_str += f"<b>➲Username:</b> {username}\n"
-    message_out_str += f"<b>➲Last Online:</b> {last_online(from_user)}\n"
-    message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
-    if message.chat.type in [enums.ChatType.SUPERGROUP, enums.ChatType.GROUP]:
+    username = f"@{user.username}" if user.username else "Not available"
+    last_name = user.last_name or "Not available"
+    dc_id = user.dc_id or "Unknown"
+
+    info_text = (
+        f"<b>➲ First Name:</b> {user.first_name}\n"
+        f"<b>➲ Last Name:</b> {last_name}\n"
+        f"<b>➲ Telegram ID:</b> <code>{user.id}</code>\n"
+        f"<b>➲ Username:</b> {username}\n"
+        f"<b>➲ Data Centre:</b> <code>{dc_id}</code>\n"
+        f"<b>➲ Last Online:</b> {last_online(user)}\n"
+        f"<b>➲ User Link:</b> <a href='tg://user?id={user.id}'>Click Here</a>\n"
+    )
+
+    # Chat join date (if applicable)
+    if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         try:
-            chat_member_p = await message.chat.get_member(from_user.id)
-            joined_date = chat_member_p.joined_date.strftime('%Y.%m.%d %H:%M:%S') if chat_member_p.joined_date else 'Not found'
-            message_out_str += (
-                "<b>➲Joined this Chat on:</b> <code>"
-                f"{joined_date}"
-                "</code>\n"
-            )
+            member = await message.chat.get_member(user.id)
+            if member.joined_date:
+                info_text += f"<b>➲ Joined This Chat:</b> <code>{member.joined_date.strftime('%Y-%m-%d %H:%M:%S')}</code>\n"
         except UserNotParticipant:
             pass
-    chat_photo = from_user.photo
-    if chat_photo:
-        local_user_photo = await client.download_media(
-            message=chat_photo.big_file_id
-        )
-        await message.reply_photo(
-            photo=local_user_photo,
-            quote=True,
-            caption=message_out_str,
-            parse_mode=enums.ParseMode.HTML,
-            disable_notification=True
-        )
-        os.remove(local_user_photo)
+
+    # Send photo if available
+    if user.photo:
+        local_photo = await client.download_media(user.photo.big_file_id)
+        await message.reply_photo(photo=local_photo, caption=info_text, parse_mode=enums.ParseMode.HTML)
+        os.remove(local_photo)
     else:
-        await message.reply_text(
-            text=message_out_str,
-            quote=True,
-            parse_mode=enums.ParseMode.HTML,
-            disable_notification=True
-        )
+        await message.reply_text(info_text, parse_mode=enums.ParseMode.HTML)
+
     await status_message.delete()
 
 
-
-def last_online(from_user):
-    time = ""
-    if from_user.is_bot:
-        time += "🤖 Bot :("
-    elif from_user.status == enums.UserStatus.RECENTLY:
-        time += "Recently"
-    elif from_user.status == enums.UserStatus.LAST_WEEK:
-        time += "Within the last week"
-    elif from_user.status == enums.UserStatus.LAST_MONTH:
-        time += "Within the last month"
-    elif from_user.status == enums.UserStatus.LONG_AGO:
-        time += "A long time ago :("
-    elif from_user.status == enums.UserStatus.ONLINE:
-        time += "Currently Online"
-    elif from_user.status == enums.UserStatus.OFFLINE:
-        time += from_user.last_online_date.strftime("%a, %d %b %Y, %H:%M:%S")
-    return time
-
+def last_online(user):
+    if user.is_bot:
+        return "🤖 Bot"
+    status = getattr(user, 'status', None)
+    if status == enums.UserStatus.ONLINE:
+        return "Currently Online"
+    elif status == enums.UserStatus.RECENTLY:
+        return "Recently"
+    elif status == enums.UserStatus.LAST_WEEK:
+        return "Within Last Week"
+    elif status == enums.UserStatus.LAST_MONTH:
+        return "Within Last Month"
+    elif status == enums.UserStatus.LONG_AGO:
+        return "A long time ago"
+    elif status == enums.UserStatus.OFFLINE:
+        return user.last_online_date.strftime("%a, %d %b %Y, %H:%M:%S") if user.last_online_date else "Offline"
+    return "Unknown"
