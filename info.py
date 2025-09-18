e6286cc3831ae3f91eab0113b10055e84cdfd8da
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-18 19:38:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,161 +1,173 @@
 import re
-from os import environ
-import os
-from Script import script
 import logging
+from os import environ
+from Script import script
 
+# ---------------- LOGGER ---------------- #
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
-def is_enabled(type, value):
-    data = environ.get(type, str(value))
-    if data.lower() in ["true", "yes", "1", "enable", "y"]:
+# ---------------- HELPERS ---------------- #
+def is_enabled(env_name: str, default: bool) -> bool:
+    """
+    Convert environment variable values into boolean flags.
+    """
+    data = environ.get(env_name, str(default)).lower()
+    if data in ["true", "yes", "1", "enable", "y"]:
         return True
-    elif data.lower() in ["false", "no", "0", "disable", "n"]:
+    elif data in ["false", "no", "0", "disable", "n"]:
         return False
     else:
-        logger.error(f'{type} is invalid, exiting now')
-        exit()
+        logger.error(f"{env_name} is invalid -> {data}. Exiting now.")
+        exit(1)
 
-def is_valid_ip(ip):
-    ip_pattern = r'\b(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
+
+def is_valid_ip(ip: str) -> bool:
+    """
+    Validate if the given string is a valid IPv4 address.
+    """
+    ip_pattern = r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." \
+                 r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." \
+                 r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\." \
+                 r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     return re.match(ip_pattern, ip) is not None
 
-# Bot information
-API_ID = environ.get('API_ID', '')
-if len(API_ID) == 0:
-    logger.error('API_ID is missing, exiting now')
-    exit()
-else:
-    API_ID = int(API_ID)
-API_HASH = environ.get('API_HASH', '')
-if len(API_HASH) == 0:
-    logger.error('API_HASH is missing, exiting now')
-    exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-if len(BOT_TOKEN) == 0:
-    logger.error('BOT_TOKEN is missing, exiting now')
-    exit()
-BOT_ID = BOT_TOKEN.split(":")[0]
-PORT = int(environ.get('PORT', '8080'))
 
-# Upload your images to "postimages.org" and get direct link
-PICS = (environ.get('PICS', 'h')).split()
+def get_required_env(name: str) -> str:
+    """
+    Fetch required environment variable or exit if missing.
+    """
+    value = environ.get(name, "").strip()
+    if not value:
+        logger.error(f"{name} is missing. Exiting now.")
+        exit(1)
+    return value
 
-# Bot Admins
-ADMINS = environ.get('ADMINS', '')
-if len(ADMINS) == 0:
-    logger.error('ADMINS is missing, exiting now')
-    exit()
-else:
-    ADMINS = [int(admins) for admins in ADMINS.split()]
 
-# Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '').split()]
-if len(INDEX_CHANNELS) == 0:
-    logger.info('INDEX_CHANNELS is empty')
-LOG_CHANNEL = environ.get('LOG_CHANNEL', '')
-if len(LOG_CHANNEL) == 0:
-    logger.error('LOG_CHANNEL is missing, exiting now')
-    exit()
-else:
-    LOG_CHANNEL = int(LOG_CHANNEL)
-    
-# support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '')
-if len(SUPPORT_GROUP) == 0:
-    logger.error('SUPPORT_GROUP is missing, exiting now')
-    exit()
-else:
-    SUPPORT_GROUP = int(SUPPORT_GROUP)
+def get_int_env(name: str, default: int) -> int:
+    """
+    Convert environment variable into int safely.
+    """
+    try:
+        return int(environ.get(name, default))
+    except ValueError:
+        logger.error(f"{name} must be an integer. Exiting now.")
+        exit(1)
 
-# MongoDB information
-DATA_DATABASE_URL = environ.get('DATA_DATABASE_URL', "")
-if len(DATA_DATABASE_URL) == 0:
-    logger.error('DATA_DATABASE_URL is missing, exiting now')
-    exit()
-FILES_DATABASE_URL = environ.get('FILES_DATABASE_URL', "")
-if len(FILES_DATABASE_URL) == 0:
-    logger.error('FILES_DATABASE_URL is missing, exiting now')
-    exit()
-SECOND_FILES_DATABASE_URL = environ.get('SECOND_FILES_DATABASE_URL', "")
-if len(SECOND_FILES_DATABASE_URL) == 0:
-    logger.info('SECOND_FILES_DATABASE_URL is empty')
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
-# Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/HA_Bots_Support')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/HA_Bots')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/HA_Films_World')
+# ---------------- BOT INFORMATION ---------------- #
+API_ID = int(get_required_env("API_ID"))
+API_HASH = get_required_env("API_HASH")
+BOT_TOKEN = get_required_env("BOT_TOKEN")
+BOT_ID = int(BOT_TOKEN.split(":")[0])
+PORT = get_int_env("PORT", 8080)
+
+# ---------------- IMAGES ---------------- #
+PICS = environ.get("PICS", "").split() or [
+    "https://i.postimg.cc/8C15CQ5y/1.png"
+]
+
+# ---------------- ADMINS ---------------- #
+ADMINS = [int(x) for x in get_required_env("ADMINS").split()]
+
+# ---------------- CHANNELS & GROUPS ---------------- #
+INDEX_CHANNELS = [
+    int(x) if x.startswith("-") else x
+    for x in environ.get("INDEX_CHANNELS", "").split()
+]
+LOG_CHANNEL = int(get_required_env("LOG_CHANNEL"))
+SUPPORT_GROUP = int(get_required_env("SUPPORT_GROUP"))
+
+# ---------------- DATABASES ---------------- #
+DATA_DATABASE_URL = get_required_env("DATA_DATABASE_URL")
+FILES_DATABASE_URL = get_required_env("FILES_DATABASE_URL")
+SECOND_FILES_DATABASE_URL = environ.get("SECOND_FILES_DATABASE_URL", "")
+
+DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+COLLECTION_NAME = environ.get("COLLECTION_NAME", "Files")
+
+# ---------------- LINKS ---------------- #
+SUPPORT_LINK = environ.get("SUPPORT_LINK", "https://t.me/HA_Bots_Support")
+UPDATES_LINK = environ.get("UPDATES_LINK", "https://t.me/HA_Bots")
+FILMS_LINK = environ.get("FILMS_LINK", "https://t.me/HA_Films_World")
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
 
-# Bot settings
-TIME_ZONE = environ.get('TIME_ZONE', 'Asia/Colombo') # Replace your time zone
-DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-MAX_BTN = int(environ.get('MAX_BTN', 8))
-LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'hindi english telugu tamil kannada malayalam marathi punjabi').split()]
-QUALITY = [quality.lower() for quality in environ.get('QUALITY', '360p 480p 720p 1080p 2160p').split()]
+# ---------------- BOT SETTINGS ---------------- #
+TIME_ZONE = environ.get("TIME_ZONE", "Asia/Colombo")
+DELETE_TIME = get_int_env("DELETE_TIME", 3600)
+CACHE_TIME = get_int_env("CACHE_TIME", 300)
+MAX_BTN = get_int_env("MAX_BTN", 8)
+
+LANGUAGES = [
+    x.lower() for x in environ.get(
+        "LANGUAGES", "hindi english telugu tamil kannada malayalam marathi punjabi"
+    ).split()
+]
+QUALITY = [
+    x.lower() for x in environ.get(
+        "QUALITY", "360p 480p 720p 1080p 2160p"
+    ).split()
+]
+
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "mdiskshortner.link")
-SHORTLINK_API = environ.get("SHORTLINK_API", "36f1ae74ba1aa01e5bd73bdd0bc22aa915443501")
-VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
-INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
-PM_FILE_DELETE_TIME = int(environ.get('PM_FILE_DELETE_TIME', '3600'))
 
-# boolean settings
-USE_CAPTION_FILTER = is_enabled('USE_CAPTION_FILTER', False)
-IS_VERIFY = is_enabled('IS_VERIFY', False)
-AUTO_DELETE = is_enabled('AUTO_DELETE', False)
-WELCOME = is_enabled('WELCOME', False)
-PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "vplink.in")
+SHORTLINK_API = environ.get("SHORTLINK_API", "")
+VERIFY_EXPIRE = get_int_env("VERIFY_EXPIRE", 86400)
+INDEX_EXTENSIONS = [
+    x.lower() for x in environ.get("INDEX_EXTENSIONS", "mp4 mkv").split()
+]
+PM_FILE_DELETE_TIME = get_int_env("PM_FILE_DELETE_TIME", 3600)
+
+# ---------------- BOOLEAN FLAGS ---------------- #
+USE_CAPTION_FILTER = is_enabled("USE_CAPTION_FILTER", False)
+IS_VERIFY = is_enabled("IS_VERIFY", False)
+AUTO_DELETE = is_enabled("AUTO_DELETE", True)
+WELCOME = is_enabled("WELCOME", False)
+PROTECT_CONTENT = is_enabled("PROTECT_CONTENT", False)
 LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
-LINK_MODE = is_enabled("LINK_MODE", True)
-IMDB = is_enabled('IMDB', True)
+LINK_MODE = is_enabled("LINK_MODE", False)
+IMDB = is_enabled("IMDB", False)
 SPELL_CHECK = is_enabled("SPELL_CHECK", True)
-SHORTLINK = is_enabled('SHORTLINK', False)
+SHORTLINK = is_enabled("SHORTLINK", False)
 
-# for stream
-IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "")
-if len(BIN_CHANNEL) == 0:
-    logger.error('BIN_CHANNEL is missing, exiting now')
-    exit()
+# ---------------- STREAMING ---------------- #
+IS_STREAM = is_enabled("IS_STREAM", False)
+BIN_CHANNEL = int(get_required_env("BIN_CHANNEL"))
+
+URL = get_required_env("URL")
+if URL.startswith(("https://", "http://")):
+    if not URL.endswith("/"):
+        URL += "/"
+elif is_valid_ip(URL):
+    URL = f"http://{URL}/"
 else:
-    BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
-if len(URL) == 0:
-    logger.error('URL is missing, exiting now')
-    exit()
-else:
-    if URL.startswith(('https://', 'http://')):
-        if not URL.endswith("/"):
-            URL += '/'
-    elif is_valid_ip(URL):
-        URL = f'http://{URL}/'
-    else:
-        logger.error('URL is not valid, exiting now')
-        exit()
+    logger.error("URL is not valid. Exiting now.")
+    exit(1)
 
-#start command reactions and sticker
-REACTIONS = [reactions for reactions in environ.get('REACTIONS', '🤝 😇 🤗 😍 👍 🎅 😐 🥰 🤩 😱 🤣 😘 👏 😛 😈 🎉 ⚡️ 🫡 🤓 😎 🏆 🔥 🤭 🌚 🆒 👻 😁').split()]  # Multiple reactions can be used separated by space
-STICKERS = [sticker for sticker in environ.get('STICKERS', 'CAACAgIAAxkBAAEN4ctnu1NdZUe21tiqF1CjLCZW8rJ28QACmQwAAj9UAUrPkwx5a8EilDYE CAACAgIAAxkBAAEN1pBntL9sz1tuP_qo0bCdLj_xQa28ngACxgEAAhZCawpKI9T0ydt5RzYE').split()]  # Multiple sticker can be used separated by space, use @idstickerbot for get sticker id
+# ---------------- REACTIONS & STICKERS ---------------- #
+REACTIONS = environ.get(
+    "REACTIONS",
+    "🤝 😇 🤗 😍 👍 🎅 😐 🥰 🤩 😱 🤣 😘 👏 😛 😈 🎉 ⚡️ 🫡 🤓 😎 🏆 🔥 🤭 🌚 🆒 👻 😁"
+).split()
 
+STICKERS = environ.get(
+    "STICKERS",
+    "CAACAgIAAxkBAAEN4ctnu1NdZUe21tiqF1CjLCZW8rJ28QACmQwAAj9UAUrPkwx5a8EilDYE "
+    "CAACAgIAAxkBAAEN1pBntL9sz1tuP_qo0bCdLj_xQa28ngACxgEAAhZCawpKI9T0ydt5RzYE"
+).split()
 
-# for Premium 
-IS_PREMIUM = is_enabled('IS_PREMIUM', True)
-PRE_DAY_AMOUNT = int(environ.get('PRE_DAY_AMOUNT', '10')) # add amount in INR for premium charge pre day 
+# ---------------- PREMIUM ---------------- #
+IS_PREMIUM = is_enabled("IS_PREMIUM", False)
+PRE_DAY_AMOUNT = get_int_env("PRE_DAY_AMOUNT", 10)
+
 UPI_ID = environ.get("UPI_ID", "")
-if len(UPI_ID) == 0:
-    logger.info('UPI_ID is empty')
-UPI_NAME = environ.get("UPI_NAME", "") # add your UPI account name
-if len(UPI_NAME) == 0:
-    logger.info('UPI_NAME is empty')
+UPI_NAME = environ.get("UPI_NAME", "")
 RECEIPT_SEND_USERNAME = environ.get("RECEIPT_SEND_USERNAME", "@Hansaka_Anuhas")
-if len(UPI_ID) == 0 or len(UPI_NAME) == 0:
-    logger.info('IS_PREMIUM disabled due to empty UPI_ID or UPI_NAME')
+
+if not UPI_ID or not UPI_NAME:
+    logger.warning("IS_PREMIUM disabled due to missing UPI details.")
     IS_PREMIUM = False
-    
