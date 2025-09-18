318b385ca9488a826983672a7dc48f16b824c60e
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-18 21:43:39 +0530

    Update group_management.py

--- plugins/group_management.py
+++ plugins/group_management.py
@@ -1,114 +1,37 @@
 from hydrogram import Client, filters
-from hydrogram.types import ChatPermissions, InlineKeyboardMarkup, InlineKeyboardButton
+from hydrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import is_check_admin
 
 # ---------------- Manage Button ---------------- #
 @Client.on_message(filters.command('manage') & filters.group)
 async def members_management(client, message):
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await message.reply_text('You are not admin in this group.')
-    
+        return await message.reply_text('You are not an admin in this group.')
+
     btn = [
-        [InlineKeyboardButton('Unmute All', callback_data='unmute_all_members'),
-         InlineKeyboardButton('Unban All', callback_data='unban_all_members')],
-        [InlineKeyboardButton('Kick Muted Users', callback_data='kick_muted_members'),
-         InlineKeyboardButton('Kick Deleted Accounts', callback_data='kick_deleted_accounts_members')]
+        [InlineKeyboardButton('Kick Deleted Accounts', callback_data='kick_deleted_accounts_members')]
     ]
     await message.reply_text(
-        "Select one of the functions to manage members.",
+        "Select a function to manage members.",
         reply_markup=InlineKeyboardMarkup(btn)
     )
 
-# ---------------- Ban Command ---------------- #
-@Client.on_message(filters.command('ban') & filters.group)
-async def ban_chat_user(client, message):
+# ---------------- Kick Deleted Accounts ---------------- #
+@Client.on_message(filters.command('kickdeleted') & filters.group)
+async def kick_deleted_accounts(client, message):
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await message.reply_text('You are not admin in this group.')
+        return await message.reply_text('You are not an admin in this group.')
 
-    # Get target user
-    if message.reply_to_message and message.reply_to_message.from_user:
-        user_id = message.reply_to_message.from_user.id
-    else:
-        try:
-            user_id = message.text.split(" ", 1)[1]
-        except IndexError:
-            return await message.reply_text("Reply to a user or provide user ID/username.")
-    
-    try:
-        user_id = int(user_id)
-    except ValueError:
-        pass
+    members = await client.get_chat_members(message.chat.id)
+    kicked_count = 0
 
-    try:
-        user = (await client.get_chat_member(message.chat.id, user_id)).user
-    except:
-        return await message.reply_text("Cannot find this user in the group.")
+    for member in members:
+        if member.user.is_deleted:
+            try:
+                await client.ban_chat_member(message.chat.id, member.user.id)
+                await client.unban_chat_member(message.chat.id, member.user.id)
+                kicked_count += 1
+            except:
+                continue
 
-    try:
-        await client.ban_chat_member(message.chat.id, user_id)
-    except:
-        return await message.reply_text("I don't have permission to ban this user.")
-
-    await message.reply_text(f'Successfully banned {user.mention} from {message.chat.title}.')
-
-# ---------------- Mute Command ---------------- #
-@Client.on_message(filters.command('mute') & filters.group)
-async def mute_chat_user(client, message):
-    if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await message.reply_text('You are not admin in this group.')
-
-    if message.reply_to_message and message.reply_to_message.from_user:
-        user_id = message.reply_to_message.from_user.id
-    else:
-        try:
-            user_id = message.text.split(" ", 1)[1]
-        except IndexError:
-            return await message.reply_text("Reply to a user or provide user ID/username.")
-
-    try:
-        user_id = int(user_id)
-    except ValueError:
-        pass
-
-    try:
-        user = (await client.get_chat_member(message.chat.id, user_id)).user
-    except:
-        return await message.reply_text("Cannot find this user in the group.")
-
-    try:
-        await client.restrict_chat_member(message.chat.id, user_id, ChatPermissions())
-    except:
-        return await message.reply_text("I don't have permission to mute this user.")
-
-    await message.reply_text(f'Successfully muted {user.mention} in {message.chat.title}.')
-
-# ---------------- Unban / Unmute ---------------- #
-@Client.on_message(filters.command(["unban", "unmute"]) & filters.group)
-async def unban_chat_user(client, message):
-    if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await message.reply_text('You are not admin in this group.')
-
-    if message.reply_to_message and message.reply_to_message.from_user:
-        user_id = message.reply_to_message.from_user.id
-    else:
-        try:
-            user_id = message.text.split(" ", 1)[1]
-        except IndexError:
-            return await message.reply_text("Reply to a user or provide user ID/username.")
-
-    try:
-        user_id = int(user_id)
-    except ValueError:
-        pass
-
-    try:
-        user = (await client.get_chat_member(message.chat.id, user_id)).user
-    except:
-        return await message.reply_text("Cannot find this user in the group.")
-
-    try:
-        await client.unban_chat_member(message.chat.id, user_id)
-    except:
-        return await message.reply_text(f"I don't have permission to {message.command[0]} this user.")
-
-    await message.reply_text(f'Successfully {message.command[0]} {user.mention} in {message.chat.title}.')
+    await message.reply_text(f"✅ Kicked {kicked_count} deleted accounts from {message.chat.title}.")
