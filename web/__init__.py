83cd5a01b7b70f25b5e4ef912d973dfd033c0c34
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-19 09:58:34 +0530

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,2 @@
-from aiohttp import web
-
-web_app = web.Application()
-
-async def hello(request):
-    return web.Response(text="Bot is running!")
-
-web_app.add_routes([web.get("/", hello)])
+# Initialize the web app
+from .app import web_app
