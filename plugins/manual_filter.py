ec019219f8501b63bbd272d2d8eca57ac7625634
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-19 09:47:29 +0530

    Update manual_filter.py

--- plugins/manual_filter.py
+++ plugins/manual_filter.py
@@ -2,7 +2,7 @@ import re
 import asyncio
 from hydrogram import Client, filters
 from hydrogram.types import InlineKeyboardMarkup
-from database import filters_col  # MongoDB collection for filters
+from database.ia_filterdb import filters_col
 from config import AUTO_DELETE, ADMINS
 
 # ------------------------------
